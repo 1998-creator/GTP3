6720f3966a99c27ec5b84ae1e6f42c0487190f86
Payton  Provo <paytonprovo1@gmail.com>
2021-11-16 00:49:03 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+import "./App.css";
 
 const App = () => {
-    return (
-        <div>
-           <h1> App </h1>
-        </div>
-    )
-}
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
